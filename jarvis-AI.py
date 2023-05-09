21ff11f6f5a4bb91bd9d77a510dd246aee5700ce
saadansari234 <saadansari3212@gmail.com>
2023-05-09 18:42:31 +0530

    introducing syntax in python

---
+++ jarvis-AI.py
@@ -0,0 +1,43 @@
+print ("practice module to understand how syntax work in python")
+print ("hello world this is saad ansari")
+
+print ("example 1")
+
+x = 5
+y = "saad ansari"
+print (x)
+print (y)
+
+print ("example 2")
+x = 5
+x = "saad ansari"
+print (x)
+print (x)
+
+print ("example 3")
+x = 5
+X = "saad ansari"
+print (x)
+print (X)
+
+print ("example 4")
+print ("int use for numbers only")
+a = int(7)
+print ("ste user for words only")
+b = str("saad ansari")
+print ("float use for numbers with dot")
+c = (9.0)
+print (a)
+print (b)
+print (c)
+
+
+print ("example 5")
+
+
+a = (7)
+b = ("hello saad")
+c = (9.0)
+print (type(a))
+print (type(b))
+print (type(c))
